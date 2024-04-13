9eb96807c5c2c13d671ce9e1d7ab45463a99b247
Ayush Raj <ardubey1504@gmail.com>
2024-04-13 20:46:03 +0530

    bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,7 +42,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Password is required']
     },
-    refreshToke: {
+    refreshToken: {
       type: String,
     }
 
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 // this will encrypt the password when password is modified
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next(); 
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
