25a123d21fe8046ac775eedda6b13bcf2984364d
Ayush Raj <ardubey1504@gmail.com>
2024-04-18 14:57:27 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/apiError.js";
+import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponce.js";
@@ -93,7 +93,8 @@ const loginUser = asyncHandler(async (req, res) => {
   const {email, username, password} = req.body
 
   // usrename or email
-  if(!username || !email){
+  // if(!username && !email){}
+  if(!(username || email)){
     throw new ApiError(400, "username or password is required");
   }
 
